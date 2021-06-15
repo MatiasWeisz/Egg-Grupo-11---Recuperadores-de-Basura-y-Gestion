2bfa58712628da3bb3ff8b4da2ace9355fff3c72
MatiasWeisz <matiasweisz1@gmail.com>
2021-06-14 21:35:36 -0300

    Creacion del Proyecto

---
+++ ProyectoEgg/src/main/java/Grupo11/ProyectoEgg/ProyectoEggApplication.java
@@ -0,0 +1,13 @@
+package Grupo11.ProyectoEgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEggApplication.class, args);
+	}
+
+}
---
+++ ProyectoEgg/src/test/java/Grupo11/ProyectoEgg/ProyectoEggApplicationTests.java
@@ -0,0 +1,13 @@
+package Grupo11.ProyectoEgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
